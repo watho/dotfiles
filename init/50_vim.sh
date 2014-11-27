0a7314455a56cca77048b16ccf3125e6f2c004db
Thomas Wagner <wathosoft@gmx.net>
2014-11-27 23:13:31 +0100

    Checkout von NeoBundle hinzugefügt

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -1,7 +1,11 @@
 # Backups, swaps and undos are stored here.
 mkdir -p $DOTFILES/caches/vim
+mkdir -p $DOTFILES/.link/bundle
+
+git clone https://github.com/Shougo/neobundle.vim $DOTFILES/.lin/bundle/neobundle.vim
+
 
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
-  vim +PlugUpgrade +PlugUpdate +qall
+  vim +NeoBundleCheck
 fi
