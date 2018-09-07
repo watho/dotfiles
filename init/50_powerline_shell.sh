5dd62bf001c7cad779e9e19999b96ffb624e6a9e
Thomas Wagner <wathosoft@gmx.net>
2018-09-07 13:43:00 +0200

    powerline config

--- init/50_powerline_shell.sh
+++ init/50_powerline_shell.sh
@@ -1,8 +1,2 @@
 #
-#mkdir -p $DOTFILES/link/powerline-shell
-
-#git clone https://github.com/milkbikis/powerline-shell $DOTFILES/link/powerline-shell
-#cd $DOTFILES/link/powerline-shell
-#python $DOTFILES/link/powerline-shell/install.py
-pip install powerline-status
-
+sudo apt install powerline
